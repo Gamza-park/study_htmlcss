163c180590af721cdca00f3bd1e2ddef408e00b3
박형일 <gamza@baghyeong-il-ui-MacBookPro.local>
2020-06-11 13:45:11 +0900

    Add script.js

---
+++ sample2/javascript/script.js
@@ -0,0 +1,8 @@
+wbtn = document.getElementById('white_btn');
+wbtn.addEventListener('click', function(){
+    document.getElementById('c').className='white'
+})
+bbtn = document.getElementById('black_btn');
+bbtn.addEventListener('click', function(){
+    document.getElementById('c').className='black'
+})
