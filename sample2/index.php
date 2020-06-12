9201e2f6f18ba9a9cdd38337d9e58c9f3083b908
박형일 <gamza@baghyeong-il-ui-MacBookPro.local>
2020-06-12 15:47:28 +0900

    Security

--- sample2/index.php
+++ sample2/index.php
@@ -18,7 +18,7 @@ $result = mysqli_query($conn, 'SELECT*FROM topic');
         <ol>
         <?php
         while($row = mysqli_fetch_assoc($result)){
-            echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>';
+            echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
         }
         ?>
         </ol>
@@ -31,8 +31,9 @@ $result = mysqli_query($conn, 'SELECT*FROM topic');
             $sql = 'SELECT*FROM topic WHERE id='.$_GET['id'];
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_assoc($result);
-            echo '<h2>'.$row['title'].'</h2>';
-            echo $row['description'];
+            echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+            echo '<p>'.htmlspecialchars($row['name']).'</p>';
+            echo strip_tags($row['description'],'<a><h1><h2><h3><h4><h5><ul><ol><li>');
         }
         ?>
     </article>
